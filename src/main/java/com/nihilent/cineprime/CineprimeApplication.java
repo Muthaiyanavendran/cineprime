64d01e263d5fd72e49bcba491cc3827bbe9c532d
Muthaiyanavendran K N <muthaiyanavendran.n@nihilent.com>
2025-04-24 23:18:15 +0530

    init;base Spring Boot Project structure

---
+++ src/main/java/com/nihilent/cineprime/CineprimeApplication.java
@@ -0,0 +1,13 @@
+package com.nihilent.cineprime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineprimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineprimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nihilent/cineprime/CineprimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nihilent.cineprime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineprimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
